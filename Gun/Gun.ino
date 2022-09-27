44cb3db9686cb31f271b6f5093752bf9ca064af0
dommilosz <dommilosz@gmail.com>
2022-09-27 14:13:52 +0200

    Turn laser off if point packet received

--- Gun/Gun.ino
+++ Gun/Gun.ino
@@ -8,7 +8,6 @@
 #define LASER_ACTIVE_STATE HIGH
 
 ButtonDebounce button(FIRE_BUTTON_PIN, 100);
-Adafruit_NeoPixel pixels(1, 5, NEO_GRB + NEO_KHZ800);
 
 struct IDPacket {
   int clientId;
@@ -45,19 +44,13 @@ constexpr int GetUUID() {
 
 AsyncUDP udp;
 long lastKA = 0;
+bool inFireMode = false;
 
 void ConnectToWiFi() {
-  pixels.begin();
-  pixels.clear();
-  pixels.setPixelColor(0, pixels.Color(150, 150, 0));
-  pixels.show();
-
   WiFi.begin("ESP32", "123456789");
 
   for (int i = 0; WiFi.status() != WL_CONNECTED; i++) {
     if (i > 20) {
-      pixels.clear();
-      pixels.show();
       digitalWrite(LASER_PIN, HIGH);
       esp_deep_sleep_start();
     }
@@ -69,17 +62,14 @@ void ConnectToWiFi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  pixels.clear();
-  pixels.show();
 }
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   pinMode(FIRE_BUTTON_PIN, INPUT_PULLUP);
   pinMode(LASER_PIN, OUTPUT);
   digitalWrite(LASER_PIN, !LASER_ACTIVE_STATE);
-  Serial.begin(115200);
   button.setCallback(buttonChanged);
   randomSeed(GetUUID());
 
@@ -95,6 +85,9 @@ void setup() {
       NullPacket _p = *(NullPacket *)data;
       if (_p.pktype == 0) {
         PointPacket p = *(PointPacket *)data;
+        if (inFireMode) {
+          inFireMode = false;
+        }
       }
       if (_p.pktype == 1) {
         KeepAlivePacket p = *(KeepAlivePacket *)data;
@@ -108,7 +101,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   button.update();
 
-  if ((millis() - lastKA) > 15000 && WiFi.status() != WL_CONNECTED) {
+  if ((millis() - lastKA) > 15000 || WiFi.status() != WL_CONNECTED) {
     WiFi.disconnect();
     ConnectToWiFi();
   }
@@ -122,16 +115,20 @@ void buttonChanged(const int state) {
 }
 
 
-
 void fire() {
   Serial.println("FIRE!");
   IDPacket p;
   p.clientId = GetUUID();
   p.shotId = random(60000) + 5000;
   udp.writeTo((uint8_t *)&p, sizeof(IDPacket), WiFi.gatewayIP(), 19701);
-  delay(10);
+  inFireMode = true;
   digitalWrite(LASER_PIN, LASER_ACTIVE_STATE);
-  delay(100);
+  for (int i = 0; i < 6; i++) {
+    if (!inFireMode) {
+      break;
+    }
+    delay(25);
+  }
   digitalWrite(LASER_PIN, !LASER_ACTIVE_STATE);
   Serial.println("AFTER FIRE!");
 }
