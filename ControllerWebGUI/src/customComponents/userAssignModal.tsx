79798ebd3a615c65d8022e567e6f52d6c3f923e3
dommilosz <dommilosz@gmail.com>
2022-12-10 22:39:23 +0100

    MUI Components in user assign modal

--- ControllerWebGUI/src/customComponents/userAssignModal.tsx
+++ ControllerWebGUI/src/customComponents/userAssignModal.tsx
@@ -1,8 +1,14 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import 'reactjs-popup/dist/index.css';
 import "./userAssignModal.css"
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import {Select} from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -24,7 +30,7 @@ export function UserAssignModal({
                                 }: { elements: { name: string, value: string }[], callback: (value: string) => any, open: boolean, setOpen: any }) {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    let selectRef = useRef(null);
+    const [selectedUser, setSelectedUser] = useState("");
 
     return (<Modal
             open={open}
@@ -32,33 +38,42 @@ export function UserAssignModal({
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={style}>
-                <select ref={selectRef}>
-                    {elements.map(el => {
-                        return <option value={el.value}>{el.name}</option>
-                    })}
-                </select>
-                <button
-                    className="button"
+            <Paper sx={style}>
+                <div className={"settings-text2"}>Select user to assign with this client</div>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: "100%" }}>
+                    <InputLabel id="user-select-label">User</InputLabel>
+                    <Select
+                        labelId="user-select-label"
+                        label={"User"}
+                        onChange={(e)=>{{
+                            setSelectedUser(String(e.target.value));
+                        }}}
+                    >
+                        {elements.map(el=>{
+                            return <MenuItem value={el.value}>{el.name}</MenuItem>
+                        })}
+                    </Select>
+                </FormControl>
+                <Button variant="contained" color="success"
+                        className="button" style={{margin:5}}
                     onClick={() => {
                         console.log('modal closed ');
-                        // @ts-ignore
-                        callback(selectRef.current.value)
+                        callback(selectedUser)
                         setOpen(false);
                     }}
                 >
                     Save
-                </button>
-                <button
-                    className="button"
+                </Button>
+                <Button variant="contained" color="error"
+                    className="button" style={{margin:5}}
                     onClick={() => {
                         console.log('modal closed ');
                         setOpen(false);
                     }}
                 >
                     Cancel
-                </button>
-            </Box>
+                </Button>
+            </Paper>
         </Modal>
     );
 }
