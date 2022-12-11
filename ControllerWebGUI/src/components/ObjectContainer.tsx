bc9bced9ab68a13f3e27324854aec517840f066b
dommilosz <dommilosz@gmail.com>
2022-12-11 13:41:48 +0100

    Add session settings element in object container when session is empty

--- ControllerWebGUI/src/components/ObjectContainer.tsx
+++ ControllerWebGUI/src/components/ObjectContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SessionSetting from "./SettingItems/SessionSetting";
 
 export default function ({children,empty}:{children:any,empty:boolean}){
     if(empty){
         return <div className="object-container">
             Session is empty. Shot some shots or load previous session to begin.
+            <SessionSetting></SessionSetting>
         </div>
     }
     return <div className="object-container">
