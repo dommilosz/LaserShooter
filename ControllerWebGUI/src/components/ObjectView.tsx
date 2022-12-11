686b13dce015fb4903c4d091f4d683faff434ce4
dommilosz <dommilosz@gmail.com>
2022-12-11 12:53:28 +0100

    Make object-containers shrink more on mobile

--- ControllerWebGUI/src/components/ObjectView.tsx
+++ ControllerWebGUI/src/components/ObjectView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function ({children}:{children:any}){
-    return <div style={{ display: "flex", width: "33%", minWidth:300 }}>
+    return <div style={{ display: "flex", width: "33%", minWidth:200 }}>
         {children}
     </div>
 }
