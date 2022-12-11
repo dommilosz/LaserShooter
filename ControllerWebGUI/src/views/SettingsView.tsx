3d5285fcfb4338a0a16ba64bd1f5eabd6b280cf0
dommilosz <dommilosz@gmail.com>
2022-12-11 12:51:14 +0100

    Add overflow to settings

--- ControllerWebGUI/src/views/SettingsView.tsx
+++ ControllerWebGUI/src/views/SettingsView.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "./SettingsView.css"
-import { styled } from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import ServerSetting from "../components/SettingItems/ServerSetting";
@@ -8,30 +8,32 @@ import SessionSetting from "../components/SettingItems/SessionSetting";
 import ShowAllOnTargetSetting from "../components/SettingItems/ShowAllOnTargetSetting";
 import ResetClient from "../components/SettingItems/ResetClient";
 
-export const SettingItem = styled(Paper)(({ theme }) => ({
+export const SettingItem = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    height:200,
+    height: 200,
 }));
 
 export default function SettingsView() {
-    return <div id="settings-view">
-        <Grid container spacing={2}>
-            <Grid item xs={4}>
-                <ServerSetting/>
+    return <div style={{overflow:"auto"}}>
+        <div id="settings-view">
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <ServerSetting/>
+                </Grid>
+                <Grid item xs={4}>
+                    <SessionSetting/>
+                </Grid>
+                <Grid item xs={4}>
+                    <ShowAllOnTargetSetting/>
+                </Grid>
+                <Grid item xs={4}>
+                    <ResetClient/>
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <SessionSetting/>
-            </Grid>
-            <Grid item xs={4}>
-                <ShowAllOnTargetSetting/>
-            </Grid>
-            <Grid item xs={4}>
-                <ResetClient/>
-            </Grid>
-        </Grid>
+        </div>
     </div>
 }
