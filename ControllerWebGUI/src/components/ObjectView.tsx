282c36cd980fefc1d183d0de698da80f3dfbc650
dommilosz <dommilosz@gmail.com>
2023-01-08 14:08:37 +0100

    Fix x overflow in object container

--- ControllerWebGUI/src/components/ObjectView.tsx
+++ ControllerWebGUI/src/components/ObjectView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Paper from "@mui/material/Paper";
 
 export default function ({children}:{children:any}){
-    return <Paper style={{ display: "flex", width: "33%", minWidth:200, height:"100%" }}>
+    return <Paper style={{ display: "flex", width: "33%", minWidth:200, height:"100%"}}>
         {children}
     </Paper>
 }
